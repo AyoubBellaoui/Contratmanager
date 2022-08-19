868057e2cce6d722d9934cdf274ed0846cfc3ff2
Ayoub Bellaoui <89253085+AyoubBellaoui@users.noreply.github.com>
2022-08-19 22:40:14 +0100

    Add files via upload

---
+++ Includes/Links.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../Static/JEQUERY.JS"></script>
+    <script src="../Static/selectize.min.js"></script>
+    <script src="../Static/bootstrap.min.js"></script>
+    <script src="../Static/popper.min.js"></script>
+    <script src="../Static/bootstrap.bundle.js"></script>
+    <script src="../Static/main.js"></script>
+    <link rel="icon" href="../Images/favicon.ico" type="image/ico">
+    <link rel='stylesheet' type='text/css' media='screen' href="../Static/selectize.bootstrap3.min.css">
+    <link rel='stylesheet' type='text/css' media='screen' href="../Static/main.css">
+    <link rel='stylesheet' type='text/css' media='screen' href='../Static/bootstrap.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../Static/Normalize.css'>
+    <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css?v9" rel="stylesheet">
+</head>
+</html>
---
+++ Includes/Nav.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../Interfaces/Home.php"> تدبير العقود بالمعهد</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="../Interfaces/Contrats.php">العقود</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="../Interfaces/Contractants.php">المتعاقدين</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="../Interfaces/Types.php">نوعية العقود</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="../Interfaces/Profile.php">الملف الشخصي</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="../Interfaces/Logout.php" onclick="return confirm('هل أنت متأكد أنك تريد تسجيل الخروج')">تسجيل خروج</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
